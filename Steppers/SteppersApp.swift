78b53cd1fcadbea58a7d87801b9b8e081c48aafd
Edwin <edwin12sv@gmail.com>
2023-12-25 19:09:47 -0600

    Initial Commit

---
+++ Steppers/SteppersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteppersApp.swift
+//  Steppers
+//
+//  Created by elsalvador on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SteppersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
